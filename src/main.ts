cea264d478d551d432c5e9288dffb027130952a8
ArakiTakaki09 <arakitakaki.work+main@gmail.com>
2025-01-15 19:55:41 +0900

    update workflow

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createElement } from 'react';
 import { App } from './App';
 import { createRoot } from 'react-dom/client';
-import 'reset-css';
+// import 'reset-css';
 const app = document.getElementById('app')
 if (app == null) throw new Error('app element not found');
 createRoot(app).render(createElement(App));
